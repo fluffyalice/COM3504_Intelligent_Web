dd557f9456792542d440f66d09444d83308a16c2
acb18fl <fli31@sheffield.ac.uk>
2022-03-18 19:54:52 +0000

    controller added, initial commit

---
+++ controllers/stories.js
@@ -0,0 +1,25 @@
+let Story = require('../models/stories');
+
+exports.getAllStories = async function(req, res) {
+    let stories = await Story.find({});
+    return res.json(stories);
+}
+
+
+exports.saveStories = function(req, res) {
+    const { photo, text, name, date } = req.body;
+    let story = new Story({
+        photo, text, name, date
+    });
+
+    story.save()
+        .then((results) => {
+            console.log("object created: " + JSON.stringify(results));
+            return res.json(results)
+        })
+        .catch((error) => {
+            console.log(JSON.stringify(error));
+            return res.status(500).json(error)
+        });
+}
+
