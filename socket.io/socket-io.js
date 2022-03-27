f2af0e7642acd367c507eb0dbf5ca147894238bc
acc19hj <hjia5@sheffield.ac.uk>
2022-03-27 00:20:58 +0000

    socket-io update

--- socket.io/socket-io.js
+++ socket.io/socket-io.js
@@ -1,9 +1,34 @@
 
 exports.init = function(io) {
-  io.sockets.on('connection', function (socket) {
-    try {
-     // insert here your event
-    } catch (e) {
-    }
-  });
+  // the app namespace
+  const chat = io
+    .of('/app')
+    .on('connection', function(socket) {
+      try {
+        /**
+         * it creates or joins a room
+         */
+        socket.on('create or join', function(room, userId) {
+          socket.join(room);
+          chat.to(room).emit('joined', room, userId);
+        });
+
+        socket.on('chat', function(room, userId, chatText) {
+          chat.to(room).emit('chat', room, userId, chatText);
+        });
+
+        socket.on('draw', function(room, data) {
+          chat.to(room).emit('draw', data);
+        });
+
+        socket.on('clear', function(room) {
+          chat.to(room).emit('clear');
+        });
+
+        socket.on('disconnect', function() {
+          console.log('someone disconnected');
+        });
+      } catch (e) {
+      }
+    });
 }
